aea36d05c6d806ae5c3c3efa779de8a24a9305a6
relluf99 <e_fuller@outlook.com>
2020-01-25 11:11:12 -0600

    2 files modified, 6 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,9 @@ import { Injectable } from '@angular/core';
 
 export class CartService {
   items = [];
+  constructor(
+    private http: HttpClient
+  ) {}
   addToCart(product) {
     this.items.push(product);
   }
@@ -18,5 +22,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 }
